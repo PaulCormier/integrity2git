5bce2087f3633443ebb50b6d94b35ca718cdea50
Paul Cormier <Paul.E.Cormier@rcmp-grc.gc.ca>
2022-02-07 11:11:53 -0500

    Add documentation

--- si_checkpoints_to_git.py
+++ si_checkpoints_to_git.py
@@ -6,10 +6,11 @@ import locale
 from datetime import datetime
 from git import Repo
 
-
+# Verify that the current directory is a git repo
 assert os.path.isdir(".git"), "Call git init first"
 
-#stdout = sys.stdout
+# Setup the output stream with the appropriate setting to be streamed into git fast-import
+
 stdout = open(sys.__stdout__.fileno(),  # no wrapper around stdout which does LF translation
               mode=sys.__stdout__.mode,
               buffering=1,
@@ -18,10 +19,18 @@ stdout = open(sys.__stdout__.fileno(),  # no wrapper around stdout which does LF
               newline='\n',
               closefd=False)
 
+# Collect command line arguments (project, date format, include/exclude filters, etc.)
+
+# Additional arguments to be passed to the si command
 additional_si_args = ""
+
+# The name of the si project being operated on
 project = sys.argv[1]
+if not project.endswith("/project.pj"):
+    project += "/project.pj"
 
 locale.setlocale(locale.LC_ALL, '')
+
 # Check for a date format passed as a parameter
 if len(sys.argv) > 2:
     if sys.argv[2] == '--date-format':
@@ -29,30 +38,38 @@ if len(sys.argv) > 2:
 else:
     date_format = '%d-%b-%Y %I:%M:%S %p'
 
+def trace(message):
+    """ Print a message to the standard error output stream. """
+    print("%s %s" % (datetime.now().strftime("%H:%M:%S"), message), file=sys.stderr)
 
-if not project.endswith("/project.pj"):
-    project += "/project.pj"
-
+### Source Integrity Methods ###
 def reencode(string):
+    """ Re-encode a string as UTF-8. """
     return string.encode("utf-8").decode(sys.__stdout__.encoding)
     
 def print_out(data):
+    """ Print the given data to the pre-configured output stream. """
     print(data, file=stdout)
 
-def trace(message):
-    print("%s %s" % (datetime.now().strftime("%H:%M:%S"), message), file=sys.stderr)
-
 def export_string(string):
+    """ 
+    Print the given string as a single "data" line in the git fast-import format.
+    The string is re-encoded as UTF-8.
+    """
     string = reencode(string)
     print_out('data %d\n%s' % (len(string), (string)))
 
 def export_data(string):
+    """
+    Print the given string as a "data" block in the git fast-import format.
+    The data is not re-encoded.
+    """
     stdout.write('data %d\n' % len(string))
     stdout.buffer.write(string)
     stdout.write('\n')
 
 def inline_data(filename, code = 'M', mode = '644'):
-
+    """ Read the given file, and print it into a "data" block in the git fast-import format. """
     content = open(filename, 'rb').read()
     if platform.system() == 'Windows':
         #this is a hack'ish way to get windows path names to work git (is there a better way to do this?)
@@ -61,6 +78,7 @@ def inline_data(filename, code = 'M', mode = '644'):
     export_data(content)
 
 def si(command):
+    """ Try to execute an si command. The result of the command is returned. """
     trace(command)
     for i in range(20):
         # subprocess.getstatusoutput() below
@@ -82,21 +100,30 @@ def si(command):
         raise Exception("Command failed")
     return data.decode("cp850")
     
-
 def retrieve_revisions(devpath=False):
+    """ 
+    Find the revisions of an si project, and return them as a list of dictionaries.
+    Each entry has: the revision number, the author, the time when the revision was created,
+    any tags associated with the revision, and the description of the revision.
+    If specified, use a development path.
+    """
+    # Quote the devpath, if specified.
     if devpath:
         devpath = '"' + devpath + '"'
     else:
         devpath = ":current"
 
+    # Find the revisions of the project
     versions = si('si viewprojecthistory %s --quiet --rfilter=devpath:%s --project="%s"' % (additional_si_args, devpath, project))
     versions = versions.split('\n')
     versions = versions[1:]
+    # A regex to match version numbers
     version_re = re.compile('^\d+(\.\d+)+\t')
 
     revisions = []
     for version in versions:
         match = version_re.match(version)
+        # If this line starts with a version number, collect the revision information
         if match:
             version_cols = version.split('\t')
             revision = {}
@@ -106,7 +133,7 @@ def retrieve_revisions(devpath=False):
             revision["tags"] = [ v for v in version_cols[5].split(",") if v ]
             revision["description"] = version_cols[6]
             revisions.append(revision)
-        else: # append to previous description
+        else: # This is the rest of the description, to be append to previous description
             if not version: continue
             if revision["description"]: revision["description"] += '\n'
             revision["description"] += version
@@ -116,6 +143,7 @@ def retrieve_revisions(devpath=False):
     return revisions
 
 def retrieve_devpaths():
+    """ Find all the development paths in the si project, and return them as a list. """
     devpaths = si('si projectinfo %s --devpaths --quiet --noacl --noattributes --noshowCheckpointDescription --noassociatedIssues --project="%s"' % (additional_si_args, project))
     devpaths = devpaths [1:]
     devpaths_re = re.compile('    (.+) \(([0-9][\.0-9]+)\)\n')
@@ -124,8 +152,12 @@ def retrieve_devpaths():
     devpath_col.sort(key=lambda x: [int(i) for i in x[1].split('.')]) #order development paths by version
     return devpath_col
 
-
 def export_to_git(revisions, done_count, devpath=False, ancestor=False, ancestorDate=None):
+    """
+    Run through the specified revisions, checkout the revision, 
+    spool the data to the git fast-import format, 
+    and create a git commit with the revision's information.
+    """
     if len(revisions) == 0: return done_count
 
     abs_sandbox_path = os.getcwd()
@@ -181,9 +213,14 @@ def export_to_git(revisions, done_count, devpath=False, ancestor=False, ancestor
     print_out('checkpoint')
     return done_count
 
+### Git Methods ###
+
+# Get the current Git repo.
 repo = Repo(os.getcwd())
 trace("Git directory: %s" % repo.common_dir)
+
 def find_continuation_point(done_count, revisions):
+    """ Pick up where this repo left off. """
     if not repo.head.is_valid(): return done_count, revisions
     last_commit_date = repo.head.commit.committed_date
     revisions2 = [r for r in revisions if r["seconds"] > last_commit_date]
@@ -194,6 +231,7 @@ def find_continuation_point(done_count, revisions):
     return done_count, revisions2
 
 def find_continuation_point_devpath(done_count, devpath2):
+    """ Pick up where this repo left off on the processing of development paths. """
     branch = [ b for b in repo.branches if b.path == "refs/heads/devpath/" + devpath2[0][0]]
     if len(branch) == 0:
         return done_count, { "info": devpath2[0], "revisions": devpath2[1] }
@@ -205,7 +243,10 @@ def find_continuation_point_devpath(done_count, devpath2):
 
 marks = {}
 def create_marks(master_revisions, devpaths3):
+    """ Create a map of revisions to marks. """
+    
     def convert_revision_to_mark(revision, allowNew, date=False):
+        """ Add an entry to the marks dictionary for the given revision. """
         if revision in marks:
             return marks[revision]
 
@@ -232,6 +273,7 @@ def create_marks(master_revisions, devpaths3):
             convert_revision_to_mark(revision["number"], True)
 
 def check_tags_for_uniqueness(all_revisions):
+    """ Confirm that all of the si tags are unique. """
     tags = {}
     for revision in all_revisions:
         for tag in revision["tags"]:
@@ -243,21 +285,32 @@ def check_tags_for_uniqueness(all_revisions):
             error = True
     assert not error, "duplicate revisions"
 
+### Main execution flow ###
+
+# The list of all si revisions
 all_revisions = retrieve_revisions()
+
+# Copy of all_revisions
 revisions = all_revisions[:]
 
+# All development paths in the si project
 devpaths = retrieve_devpaths()
+
+# Count the total number of revisions which will be converted into git commits
 devpaths2 = []
 for devpath in devpaths:
     devpath2 = (devpath, retrieve_revisions(devpath[0]))
     all_revisions.extend(devpath2[1])
     devpaths2.append(devpath2)
 total_revision_count = len(all_revisions)
+
+# Initialize the number of completed revisions
 done_count = 0
 
+# Validate the si tags on all of the revisions
 check_tags_for_uniqueness(all_revisions)
 
-
+# Validate that all development paths can be reached
 devpaths3 = []
 done_count, revisions = find_continuation_point(done_count, revisions)
 for devpath2 in devpaths2:
@@ -269,14 +322,19 @@ for devpath2 in devpaths2:
     devpath3["ancestorDate"] = ancestorDate[0]["seconds"]
     devpaths3.append(devpath3)
 trace("Found %d revisions and %d devapaths" % (len(revisions), sum([ len(dp["revisions"]) for dp in devpaths3 ]) ))
+
+# If there are no revisions to be processed, quit
 if len(revisions) == 0 and sum([ len(dp["revisions"]) for dp in devpaths3 ]) == 0:
     exit(0)
 
+# Create the dictionary of marks
 create_marks(revisions, devpaths3)
+
+# ?
 repo = None
 
+# Create a build sandbox of the first revision
 if not os.path.isdir("tmp"):
-    # Create a build sandbox of the first revision
     revision = None
     if len(revisions) > 0:
         revision = revisions[0]
@@ -288,12 +346,18 @@ if not os.path.isdir("tmp"):
 
     si('si createsandbox %s --populate --recurse --quiet --project="%s" --projectRevision=%s tmp' % (additional_si_args, project, revision["number"]))
 
+# Switch to the temporary sandbox
 os.chdir('tmp')
+
+# Begin exporting all revisions
 done_count = export_to_git(revisions, done_count) #export main branch first!!
 
+# Export all development paths
 for devpath3 in devpaths3:
     ancestor = devpath3["info"][1]
     done_count = export_to_git(devpath3["revisions"], done_count, devpath3["branchname"], ancestor, devpath3["ancestorDate"])
+
+# Switch back to the main directory
 os.chdir("..")
 
 # Drop the sandbox
